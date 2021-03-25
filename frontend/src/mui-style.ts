9b3e885f79e5ff707b2b047a625ee3ba246f2578
Palina Cetin <fpe@tut.by>
2021-03-25 23:24:06 +0300

    fix: fixed merge conflicts

--- frontend/src/mui-style.ts
+++ frontend/src/mui-style.ts
@@ -1,23 +1,23 @@
-import { lightGreen, orange } from "@material-ui/core/colors";
+import { blue } from "@material-ui/core/colors";
 import { createMuiTheme, createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 export const theme = createMuiTheme({
   palette: {
     primary: {
-      main: orange[800],
-      light: orange[600],
-      dark: orange[900]
+      main: blue[800],
+      light: blue[600],
+      dark: blue[900]
     },
     secondary: {
       main: '#e57373',
       light: '#ffa4a2',
       dark: '#af4448'
     },
-    // text: {
-    //   primary: '#212121',
-    //   secondary: 'white',
-    //   disabled: '',
-    // }
+    text: {
+      primary: '#212121',
+      secondary: 'white',
+      disabled: '',
+    }
   },
   spacing: 10,
 })
