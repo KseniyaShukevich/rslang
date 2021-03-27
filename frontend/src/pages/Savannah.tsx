d6cd365929c19f26b981dafcb989522e7b7235d8
Shukevich Kseniya <Kseniya.Shukevich@gmail.com>
2021-03-27 17:46:07 +0300

    feat: add structure for mini game

--- frontend/src/pages/Savannah.tsx
+++ frontend/src/pages/Savannah.tsx
@@ -1,27 +1,132 @@
-import React from 'react'
-import { Box } from '@material-ui/core'
-import Game from '../components/savannah/Game'
+import React, { useState } from 'react'
+import {
+         Box,
+         Container,
+         Button,
+        } from '@material-ui/core'
 import {
         Theme,
         createStyles,
         makeStyles,
       } from '@material-ui/core/styles'
+import CLOUDURL from '../constants/CLOUDURL'
+import Heart from '../components/savannah/Heart'
+import WordBtn from '../components/savannah/WordBtn'
+import { Image } from 'cloudinary-react'
+import FullscreenBtn from '../components/savannah/FullscreenBtn'
+import CloseBtn from '../components/savannah/CloseBtn'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     box: {
+      backgroundImage: `url('${CLOUDURL}/rslang/Illustration02_yokda5')`,
+      backgroundSize: 'cover',
+      backgroundPosition: '0 100%',
       height: 'calc(100vh - 40px)',
-    }
+      display: 'flex',
+      justifyContent: 'space-between',
+      flexDirection: 'column',
+      padding: 20,
+    },
+    topBox: {
+      display: 'flex',
+      justifyContent: 'flex-end'
+    },
+    containerBtn: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+    },
+    containerGif: {
+      width: 'fit-content',
+    },
+    lifes: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    button: {
+      transition: '0.5s',
+      "&:hover": {
+         backgroundColor: 'rgba(250,250,250,0.1)'
+      },
+    },
   })
 );
 
 const Savannah: React.FC = () => {
 	const classes = useStyles();
+  const [isFullscreen, setIsFullscreen] = useState<boolean>(false);
+  const [isStartLayout, setIsStartLayout] = useState<boolean>(true);
+  const [isEndLayout, setIsEndLayout] = useState<boolean>(false);
+  const words: Array<string> = ['word', 'dgfg', 'hfghfddsa', 'ghghvdcsa'];
+  const lifes: Array<number> = [0,0,0,1,1];
+
+  document.addEventListener('fullscreenchange', (event) => {
+    setIsFullscreen(!!document.fullscreenElement);
+  });
 
 	return (
-		<Box className={classes.box}>
-			<Game />
-		</Box>
+			<Box className={classes.box} id='savannah'>
+        <Box className={classes.topBox}>
+          <FullscreenBtn
+            game={'savannah'}
+            isFullscreen={isFullscreen}
+          />
+          <Box className={classes.lifes}>
+            {
+              lifes.map((life, index) =>
+                <Heart
+                  life={life}
+                  key={index}
+                />
+              )
+            }
+          </Box>
+          <CloseBtn />
+        </Box>
+
+        {
+          isStartLayout && (
+            <div style={{color: 'white', textAlign: 'center'}}>
+              START LAYOUT
+              <Button color='primary' onClick={() => {setIsStartLayout(false)}}>
+                Начать игру
+              </Button>
+            </div>
+          )
+        }
+
+        {
+          (!isStartLayout && !isEndLayout) && (
+            <>
+              <Container maxWidth='md' className={classes.containerBtn}>
+                {
+                  words.map((word, index) =>
+                    <WordBtn
+                      word={word}
+                      number={index + 1}
+                      key={index}
+                    />
+                  )
+                }
+              </Container>
+              <Button color='primary' onClick={() => {setIsEndLayout(true)}}>
+                Закончилась игра
+              </Button>
+              <Container className={classes.containerGif}>
+                <Image publicId="rslang/33Ho_by5kqq" width="90" />
+              </Container>
+            </>
+          )
+        }
+
+        {
+          isEndLayout && (
+            <div style={{color: 'white', textAlign: 'center'}}>END LAYOUT</div>
+          )
+        }
+
+      </Box>
 	);
 }
 
