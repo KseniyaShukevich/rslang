edfc9109b15c2ab24a214d51c0162aac5039de52
Shukevich Kseniya <Kseniya.Shukevich@gmail.com>
2021-03-29 21:38:15 +0300

    feat: add game animations

--- frontend/src/components/WordBtn.tsx
+++ frontend/src/components/WordBtn.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Typography } from '@material-ui/core'
 import {
         Theme,
         createStyles,
         makeStyles,
       } from '@material-ui/core/styles'
+import { IWord } from '../interfaces'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    wrong: {
+      padding: 10,
+      margin: 20,
+      background: 'rgba(50, 0, 0, 0.5)',
+      color: 'white',
+      transition: 'background 0.2s',
+      zIndex: 10,
+    },
     box: {
       padding: 10,
       margin: 20,
       color: 'white',
       transition: 'background 0.5s',
+      zIndex: 10,
       '&:hover': {
         cursor: 'pointer',
         background: 'rgba(250,250,250,0.1)',
@@ -22,20 +32,43 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 interface IProps {
-  word: string,
+  successAnimation: () => void,
+  failAnimation: () => void,
+  corrWord: IWord | null,
+  word: IWord,
   number: number,
 }
 
-const WordBtn: React.FC<IProps> = ({ word, number }: IProps) => {
-  const f = 7;
+const WordBtn: React.FC<IProps> = ({
+  successAnimation,
+  failAnimation,
+  corrWord,
+  word,
+  number ,
+}: IProps) => {
   const classes = useStyles();
+  const [isWrong, setIsWrong] = useState<boolean>(false);
+
+  const handleClick = () => {
+    if (corrWord && (word.id === corrWord.id)) {
+      successAnimation();
+    } else {
+      failAnimation();
+      setIsWrong(true);
+      setTimeout(() => setIsWrong(false), 500);
+    }
+  }
 
   return (
-    <Typography variant='h6' className={classes.box}>
+    <Typography
+      variant='h6'
+      className={isWrong ? classes.wrong : classes.box}
+      onClick={handleClick}
+    >
       <span style={{fontSize: 14, paddingRight: 7, opacity: 0.7}}>
         {number}
       </span>
-      {word}
+      {word.wordTranslate}
     </Typography>
   );
 }
--- frontend/src/pages/Savannah.tsx
+++ frontend/src/pages/Savannah.tsx
@@ -19,18 +19,37 @@ import GameLayout from '../components/GameLayout'
 import { fetchWords, selectWords } from '../slices/wordsSlice'
 import { useSelector, useDispatch } from 'react-redux'
 import { getWordsForGame } from '../generationGameWords'
+import { IWord } from '../interfaces'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     box: {
       backgroundImage: `url('${CLOUDURL}/rslang/Illustration02_yokda5')`,
-      backgroundSize: 'cover',
       backgroundPosition: '0 100%',
+      backgroundSize: 'cover',
+      transition: '0.5s',
       height: 'calc(100vh - 40px)',
       display: 'flex',
       justifyContent: 'space-between',
       flexDirection: 'column',
       padding: 20,
+    },
+    word: {
+      opacity: 0,
+      position: 'absolute',
+      color: 'white',
+      fontSize: 50,
+      fontWeight: 'lighter',
+      transition: 'opacity 7s, top 0.1s',
+    },
+    rectangle: {
+      width: 4,
+      height: 30,
+      background: 'white',
+      borderRadius: '50%',
+      position: 'absolute',
+      display: 'none',
+      transition: '0.3s'
     },
     topBox: {
       display: 'flex',
@@ -62,43 +81,114 @@ const Savannah: React.FC = () => {
   const [isFullscreen, setIsFullscreen] = useState<boolean>(false);
   const [isEndLayout, setIsEndLayout] = useState<boolean>(false);
   const [isStartLayout, setIsStartLayout] = useState<boolean>(true);
-  const words: Array<string> = ['word', 'dgfg', 'hfghfddsa', 'ghghvdcsa'];
+  const [currWord, setCurrWord] = useState<IWord | null>(null);
+  const [bgPosition, setBgPosition] = useState<number>(100);
+  const [step, setStep] = useState<number>(0);
+  const [top, setTop] = useState<number>(-15);
+  const [isStartGame, setIsStartGame] = useState<boolean>(false);
+  const [arrayWords, setArrayWords] = useState<Array<IWord> | null>(null)
   const lifes: number = 3;
 
-  const wordsArray = useSelector(selectWords);
+  const words = useSelector(selectWords);
   const dispatch = useDispatch();
 
   const generationWords = useRef<any>(null);
+  const wordEl = useRef<any>(null);
+  const container = useRef<any>(null);
+  const rectangle = useRef<any>(null);
+  // const corrBtn = useRef<any>(null);
+  const idInterval = useRef<any>(null);
 
-  useEffect(() => {
-    dispatch(fetchWords({
-      group: 0,
-      page: 0
-    }));
-  }, []);
-
-  useEffect(() => {
-    if (!isStartLayout && !isEndLayout && wordsArray) {
-      generationWords.current = getWordsForGame(wordsArray, 5);
-    }
-  }, [wordsArray, isStartLayout, isEndLayout]);
-
-  const step = () => {
+  const doStep = () => {
     if (generationWords.current) {
-      const [ word, arrayWords, func ] = generationWords.current;
-      console.log(word, arrayWords);
+      setTimeout(() => {
+        wordEl.current.style.display = '';
+        rectangle.current.style.display = '';
+      }, 300);
+      wordEl.current.style.opacity = 1;
+      const [ word, newArrWords, func ] = generationWords.current;
+      setCurrWord(word);
+      setArrayWords(newArrWords);
       generationWords.current = func();
     } else {
-      console.log('END!');
+      clearInterval(idInterval.current);
+      setIsEndLayout(true);
     }
   }
+
+  const startWord = () => {
+    idInterval.current = setInterval(() => setTop((prev) => prev + 1), 100);
+  }
+
+  const successAnimation = () => {
+    wordEl.current.style.display = 'none';
+    rectangle.current.style.display = 'block';
+    rectangle.current.style.top = `${top}%`;
+    if (rectangle.current) {
+      setTimeout(() => rectangle.current.style.top = '90%', 0);
+    }
+    setTop(-15);
+    setBgPosition((prev: number) => prev - step);
+    doStep();
+  }
+
+  const failAnimation = () => {
+    setTop(-15);
+    wordEl.current.style.opacity = 0;
+    wordEl.current.style.opacity = 0;
+    wordEl.current.style.transition = '0.3s';
+    wordEl.current.style.fontSize = '100px';
+    setTimeout(() => {
+      wordEl.current.style.fontSize = '';
+      wordEl.current.style.transition = '';
+      doStep();
+    }, 500);
+  }
+
+  useEffect(() => {
+    if (wordEl.current) {
+      wordEl.current.style.top = `${top}%`;
+    }
+    if (top === 70) {
+      failAnimation();
+    };
+  }, [top]);
+
+  useEffect(() => {
+    container.current.style.backgroundPosition = `0 ${bgPosition}%`;
+  }, [bgPosition]);
+
+  useEffect(() => {
+    if (!isStartLayout && !isEndLayout) {
+      dispatch(fetchWords({
+        group: 0,
+        page: 0
+      }));
+    }
+  }, [isStartLayout, isEndLayout]);
+
+  useEffect(() => {
+    if (!isStartLayout && !isEndLayout && words) {
+      if (idInterval.current) {
+        clearInterval(idInterval.current);
+      }
+      wordEl.current.style.opacity = 1;
+      generationWords.current = getWordsForGame(words, 5);
+      const [ word, newArrWords, func ] = generationWords.current;
+      setCurrWord(word);
+      setArrayWords(newArrWords);
+      generationWords.current = func();
+      setStep(100 / words.length);
+      startWord();
+    }
+  }, [words, isStartLayout, isEndLayout]);
 
   document.addEventListener('fullscreenchange', (event) => {
     setIsFullscreen(!!document.fullscreenElement);
   });
 
 	return (
-			<Box className={classes.box} id='savannah'>
+			<div ref={container} className={classes.box} id='savannah'>
         <Box className={classes.topBox}>
           <FullscreenBtn
             game={'savannah'}
@@ -109,34 +199,41 @@ const Savannah: React.FC = () => {
           </Box>
           <CloseBtn />
         </Box>
-          <GameLayout
-            isStartLayout={isStartLayout}
-            setIsStartLayout={setIsStartLayout}
-            isEndLayout={isEndLayout}
-            setIsEndLayout={setIsEndLayout}
-          >
-            <Container maxWidth='md' className={classes.containerBtn}>
-              {
-                words.map((word, index) =>
-                  <WordBtn
-                    word={word}
-                    number={index + 1}
-                    key={index}
-                  />
-                )
-              }
-            </Container>
-            <Button color='primary' onClick={step}>
-              Ход
-            </Button>
-            <Button color='primary' onClick={() => {setIsEndLayout(true)}}>
-              Закончилась игра
-            </Button>
-            <Container className={classes.containerGif}>
-              <Image publicId="rslang/33Ho_by5kqq" width="90" />
-            </Container>
-          </GameLayout>
-      </Box>
+        <GameLayout
+          isStartLayout={isStartLayout}
+          setIsStartLayout={setIsStartLayout}
+          isEndLayout={isEndLayout}
+          setIsEndLayout={setIsEndLayout}
+        >
+          <Container maxWidth='md' className={classes.containerBtn}>
+            <div
+              ref={wordEl}
+              className={classes.word}
+            >
+              {currWord ? currWord.wordTranslate : ''}
+            </div>
+            <div
+              ref={rectangle}
+              className={classes.rectangle}
+            />
+            {
+              arrayWords ? arrayWords.map((el, index) =>
+                <WordBtn
+                  successAnimation={successAnimation}
+                  failAnimation={failAnimation}
+                  corrWord={currWord}
+                  word={el}
+                  number={index + 1}
+                  key={index}
+                />
+              ) : 'Загрузка'
+            }
+          </Container>
+          <Container className={classes.containerGif}>
+            <Image publicId="rslang/33Ho_by5kqq" width="90" />
+          </Container>
+        </GameLayout>
+      </div>
 	);
 }
 
