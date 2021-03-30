d06fc380b3cba0570242104cb03c13844759f829
MarinaYur <marina.yurkev@gmail.com>
2021-03-30 22:36:47 +0300

    refactor: refuctor code

--- frontend/src/pages/Dictionary.tsx
+++ frontend/src/pages/Dictionary.tsx
@@ -2,20 +2,16 @@ import React from "react";
 import {
   Box,
   Button,
+  Typography,
   makeStyles,
   Theme,
-  Typography,
   List,
   ListItem,
-  Link,
 } from "@material-ui/core";
-import LocalLibraryIcon from "@material-ui/icons/LocalLibrary";
-// import classes from "*.module.css";
-import { createStyles } from "@material-ui/core/styles";
-import "../index.scss";
 import { useState } from "react";
+import LocalLibraryIcon from "@material-ui/icons/LocalLibrary";
 import background from "../assets/images/background_7.jpeg";
-import { url } from "node:inspector";
+import { createStyles } from "@material-ui/core/styles";
 
 interface IProps {
   setIsDictionary: (value: boolean) => void;
@@ -39,11 +35,56 @@ const useStyles = makeStyles((theme: Theme) =>
       display: "flex",
     },
     tab: {
+      display: "none",
+    },
+    list: {
+      display: "grid",
+      "grid-template-columns": "repeat(3, 1fr)",
+      border: "1px solid #e3f2fd",
+      boxShadow: "0 2px 4px 0 #e3f2fd",
+      margin: 0,
+      padding: 0,
+    },
+    tabs: {
+      paddingTop: "4rem",
+      height: "60vh",
+      overflowY: "auto",
+    },
+    active: {
+      fontSize: '22px',
+      fontWeight: 500,
+      padding: '6px 10px',
+      backgroundColor: "#8dc1e6",
+      "& p": {
+        margin: "0 auto",
+      },
+      [theme.breakpoints.down("xs")]: {
+        fontSize: '17px',
+      },
+    },
+    activeTab: {
       fontSize: "3.5rem",
       width: "100%",
-      textAlign: 'center',
+      textAlign: "center",
       height: "60vh",
-    margin: '0 auto',
+      margin: "0 auto",
+      [theme.breakpoints.down("xs")]: {
+        fontSize: '2.5rem',
+      },
+    },
+    listItem: {
+      fontSize: '22px',
+      fontWeight: 500,
+      padding: '6px 10px',
+      "& p": {
+        margin: "0 auto",
+      },
+      "&:hover": {
+        cursor: "pointer",
+      },
+      [theme.breakpoints.down("xs")]: {
+        fontSize: '17px',
+      },
     },
   })
 );
@@ -55,9 +96,11 @@ const Dictionary: React.FC<IProps> = ({ setIsDictionary }: IProps) => {
   const handleClick = () => {
     setIsDictionary(false);
   };
+
   const activeLink = (e: any) => {
     setTarget(e.target.innerHTML);
   };
+  const [isStudiedWords, setIsStadiedWords] = useState(false);
 
   return (
     <>
@@ -73,63 +116,49 @@ const Dictionary: React.FC<IProps> = ({ setIsDictionary }: IProps) => {
               Словарь
             </Typography>
           </Box>
-          <List
-            className="menu"
-            style={{ padding: 0, margin: 0 }}
-            onClick={activeLink}
-          >
-            <ListItem className={target == "Изучаемые слова" ? "active" : ""}>
-              <Link
-                className="link"
-                style={{ textDecoration: "none", margin: '0 auto' }}
-                href="#studied-words"
-              >
-                Изучаемые слова
-              </Link>
+          <List className={classes.list} onClick={activeLink}>
+            <ListItem
+              className={target == "Изучаемые слова" ? classes.active : classes.listItem}
+            >
+              <p>Изучаемые слова</p>
             </ListItem>
-            <ListItem className={target == "Сложные слова" ? "active" : ""}>
-              <Link
-                className="link"
-                style={{ textDecoration: "none", margin: '0 auto' }}
-                href="#difficult-words"
-              >
-                Сложные слова
-              </Link>
+            <ListItem
+              className={target == "Сложные слова" ? classes.active : classes.listItem}
+            >
+              <p>Сложные слова</p>
             </ListItem>
-            <ListItem className={target == "Удалённые слова" ? "active" : ""}>
-              <Link
-                className="link"
-                style={{ textDecoration: "none", margin: '0 auto' }}
-                href="#deleted-words"
-              >
-                Удалённые слова
-              </Link>
+            <ListItem
+              className={target == "Удалённые слова" ? classes.active : classes.listItem}
+            >
+              <p>Удалённые слова</p>
             </ListItem>
           </List>
-          <Box className="tabs">
+          <Box className={classes.tabs}>
             <Box
-            className={classes.tab}
-            id={"studied-words"}>
-              Здесь нет слов
-              <List></List>
+              className={
+                target == "Изучаемые слова" ? classes.activeTab : classes.tab
+              }
+            >
+              Здесь нет слов1
             </Box>
             <Box
-              className={classes.tab}
-              id={"difficult-words"}
+              className={
+                target == "Сложные слова" ? classes.activeTab : classes.tab
+              }
             >
-              Здесь нет слов
-              <List></List>
+              Здесь нет слов2
             </Box>
             <Box
-              className={classes.tab}
-              id={"deleted-words"}
+              className={
+                target == "Удалённые слова" ? classes.activeTab : classes.tab
+              }
             >
-              Здесь нет слов
-              <List></List>
+              Здесь нет слов3
             </Box>
           </Box>
         </Box>
       </Box>
+
       <Button onClick={handleClick}>Назад</Button>
     </>
   );
